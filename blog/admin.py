5b0637d318b0ebad64a189fb69d147ec893ee52f
kangyahua <yahua.kang@gmail.com>
2017-01-15 21:34:50 +0800

    getting started with django

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'pub_time')
+    list_filter = ('pub_time',)
+
+admin.site.register(Article, ArticleAdmin)
---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=32, default='title')
+    content = models.TextField(null=True)
+    pub_time = models.DateTimeField(null=True)
+
+    def __unicode__(self):
+        return self.title
